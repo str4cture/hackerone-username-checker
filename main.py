cdb039e3249142666caa3b6bafc7f0457ed59088
danyal <36112962+str4cture@users.noreply.github.com>
2024-09-25 19:05:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,29 @@ def check_username(username):
             return True
         else:
             return False
+            
     except requests.RequestException as e:
         print(f"Error checking username '{username}': {e}")
         return False
 
 if __name__ == "__main__":
+    
     try:
         with open('usernames.txt') as f:
             usernames = [line.rstrip() for line in f]
+            
     except FileNotFoundError:
         print("Error: 'usernames.txt' file not found.")
         sys.exit(1)
 
     available_usernames = []
+    
     for username in usernames:
+        
         if check_username(username):
             available_usernames.append(username)
             print(f"The username '{username}' is available.")
+            
             with open('available_usernames.txt', 'a') as f:
                 f.write(username + '\n')
         else:
